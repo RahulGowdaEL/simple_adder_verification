c227296b2aceea230e62f783733332f5ab410289
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2025-02-13 22:15:23 +0530

    Create sequencer.sv

---
+++ agent/sequencer.sv
@@ -0,0 +1,9 @@
+//SEQR
+
+class adder_seqr extends uvm_sequencer;
+`uvm_component_utils(adder_seqr)
+
+function new(string name = adder_seqr, uvm_component parent);
+   super.new(name,parent);
+endfunction
+endclass
