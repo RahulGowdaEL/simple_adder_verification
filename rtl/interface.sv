865ce7bbae6487c855d61e4b345a9a0b10dbeade
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2025-02-13 22:04:35 +0530

    Create interface.sv

---
+++ rtl/interface.sv
@@ -0,0 +1,17 @@
+//Interface
+interface dut_if1;
+
+//Input signals
+logic a_in;
+logic b_in;
+logic c_in;
+
+//Output_signals
+logic sum_out;
+logic carry_out;
+
+//Control Signals
+logic clock;
+logic reset;
+
+endinterface
